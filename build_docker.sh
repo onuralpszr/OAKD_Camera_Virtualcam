b78ba78927f7b59ecc57549b51b7a84330246367
Onuralp SEZER <thunderbirdtr@fedoraproject.org>
2023-03-19 13:17:39 +0300

    feat: 🚀 ubuntu and fedora dockerfiles added for local tests
    Signed-off-by: Onuralp SEZER <thunderbirdtr@fedoraproject.org>

--- build_docker.sh
+++ build_docker.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-podman build --progress=plain . -t fedora-oak:latest -f Dockerfile
+podman build --progress=plain . -t fedora-oak:latest -f docker/Dockerfile.fedora
+podman build --progress=plain . -t ubuntu-oak:latest -f docker/Dockerfile.ubuntu
